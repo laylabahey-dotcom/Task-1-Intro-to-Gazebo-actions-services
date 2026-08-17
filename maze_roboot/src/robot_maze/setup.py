31738f9db93d23148216d6aa055d11d922766aa6
layla baheyeldin <laylabahey@gmail.com>
2026-08-17 16:39:09 +0300

    Add files via upload

---
+++ maze_roboot/src/robot_maze/setup.py
@@ -0,0 +1,32 @@
+from setuptools import find_packages, setup
+
+package_name = 'robot_maze'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='layla-bahey',
+    maintainer_email='layla-bahey@todo.todo',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+        'console_scripts': [
+            'movement_x_server = robot_maze.movement_x_action_server.main:',
+            'movement_yaw_server = robot_maze.movement_yaw_action_server.main:',
+            'maze_solver = robot_maze.maze_solver.main:',
+        ],
+    },
+)
